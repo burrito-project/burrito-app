5960b889c6226dfed74e35a155dfc01b585b1c92
Paolo Flores <pflores.fisi22@gmail.com>
2024-08-12 03:11:50 -0500

    refactor top bar display

--- lib/features/map/widgets/app_top_bar.dart
+++ lib/features/map/widgets/app_top_bar.dart
@@ -1,10 +1,10 @@
-import 'package:burrito/data/entities/burrito_status.dart';
-import 'package:burrito/data/entities/last_stop_info.dart';
-import 'package:burrito/features/map/providers/bus_status_provider.dart';
-import 'package:burrito/features/map/widgets/burrito_status_badge.dart';
-import 'package:burrito/services/dio_client.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:burrito/services/dio_client.dart';
+import 'package:burrito/data/entities/burrito_status.dart';
+import 'package:burrito/data/entities/last_stop_info.dart';
+import 'package:burrito/features/map/widgets/burrito_status_badge.dart';
+import 'package:burrito/features/map/providers/bus_status_provider.dart';
 
 class BurritoTopAppBar extends ConsumerStatefulWidget {
   const BurritoTopAppBar({super.key});
@@ -46,6 +46,7 @@ class BurritoTopAppBarRenderState extends State<BurritoTopAppBarRender>
 
   @override
   void initState() {
+    // Burrito icon does boing-boing animation like a burrito ROFL
     super.initState();
     _controller = AnimationController(
       duration: const Duration(milliseconds: 300),
@@ -59,29 +60,26 @@ class BurritoTopAppBarRenderState extends State<BurritoTopAppBarRender>
 
   @override
   Widget build(BuildContext context) {
-    final hasLastStopInfo = widget.burritoState?.lastStop != null;
+    final hasLastStop = widget.burritoState?.lastStop != null;
     final pickingUp = widget.burritoState?.lastStop?.hasReached ?? false;
-    final isOff = widget.burritoState?.lastInfo.status == BusServiceStatus.off;
+    final isOff = widget.burritoState == null ||
+        widget.burritoState?.lastInfo.status == BusServiceStatus.off;
 
     return Column(
       children: [
         Container(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
           height: 90,
           color: Theme.of(context).colorScheme.primary,
+          padding: const EdgeInsets.symmetric(horizontal: 16),
           child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+              // Burrito icon and status badge
               Stack(
                 children: [
                   Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      const SizedBox(
-                        height: 44,
-                        width: 48,
-                      ),
+                      const SizedBox(height: 44, width: 48),
                       BurritoStatusBadge(
                         status: widget.burritoState != null
                             ? widget.burritoState!.lastInfo.status
@@ -95,7 +93,7 @@ class BurritoTopAppBarRenderState extends State<BurritoTopAppBarRender>
                       AnimatedBuilder(
                         animation: _animation,
                         builder: (context, child) {
-                          return pickingUp || isOff || !hasLastStopInfo
+                          return pickingUp || isOff
                               ? Container(child: child)
                               : Transform(
                                   alignment: Alignment.bottomCenter,
@@ -120,30 +118,31 @@ class BurritoTopAppBarRenderState extends State<BurritoTopAppBarRender>
                 ],
               ),
               const SizedBox(width: 15),
+              //
               Flexible(
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text(
-                      hasLastStopInfo
-                          ? pickingUp
-                              ? 'Recogiendo pasajeros...'
-                              : 'Próxima estación (${widget.burritoState!.lastStop!.distanceMeters})'
-                          : '-- --',
-                      style: TextStyle(
-                        fontSize: 15,
-                        color: pickingUp
-                            ? const Color.fromARGB(255, 97, 221, 101)
-                            : Colors.white,
+                    if (hasLastStop) ...[
+                      Text(
+                        pickingUp
+                            ? 'Recogiendo pasajeros...'
+                            : 'Próxima estación (${widget.burritoState!.lastStop!.distanceMeters})',
+                        style: TextStyle(
+                          fontSize: 15,
+                          color: pickingUp
+                              ? const Color.fromARGB(255, 97, 221, 101)
+                              : Colors.white,
+                        ),
                       ),
-                    ),
+                    ],
                     Text(
-                      hasLastStopInfo
-                          ? widget.burritoState!.lastStop!.name
-                          : isOff
-                              ? 'Fuera de servicio'
-                              : 'Esperando al burro...',
+                      isOff
+                          ? 'Fuera de servicio'
+                          : hasLastStop
+                              ? widget.burritoState!.lastStop!.name
+                              : 'Iniciando ruta...',
                       softWrap: false,
                       maxLines: 1,
                       overflow: TextOverflow.ellipsis,
@@ -162,13 +161,13 @@ class BurritoTopAppBarRenderState extends State<BurritoTopAppBarRender>
           width: double.infinity,
           height: 24,
           duration: const Duration(milliseconds: 500),
-          color: hasLastStopInfo
+          color: hasLastStop
               ? pickingUp
                   ? const Color(0xFF3E8841)
                   : Colors.red
               : const Color(0xFF3A3A3A),
           child: Center(
-            child: hasLastStopInfo
+            child: hasLastStop
                 ? pickingUp
                     ? const Text(
                         textAlign: TextAlign.center,
