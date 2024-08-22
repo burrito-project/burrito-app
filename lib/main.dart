39fb8540b5cb7afb25d71ba1b97e0fd4d32fd02b
Paolo Flores <pflores.fisi22@gmail.com>
2024-08-22 03:12:41 -0500

    ignore bad ssl certificates

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
+import 'package:universal_io/io.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:burrito/theme/burro_theme.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:burrito/features/core/pages/index.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:burrito/features/app_updates/wrappers/pending_updates_wrapper.dart';
 
 void main() {
   final widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  HttpOverrides.global = MyHttpOverrides();
 
   SystemChrome.setSystemUIOverlayStyle(
     SystemUiOverlayStyle(
@@ -20,14 +22,16 @@ void main() {
       statusBarBrightness: Brightness.dark,
     ),
   );
+
   SystemChrome.setEnabledSystemUIMode(
     SystemUiMode.edgeToEdge,
     overlays: [SystemUiOverlay.top, SystemUiOverlay.bottom],
   );
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitDown,
-    DeviceOrientation.portraitUp,
-  ]);
+
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitDown,
+  //   DeviceOrientation.portraitUp,
+  // ]);
 
   runApp(
     ProviderScope(
@@ -43,4 +47,13 @@ void main() {
       ),
     ),
   );
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
 }
